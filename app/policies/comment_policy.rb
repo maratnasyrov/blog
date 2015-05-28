b6b1939fa92ef918de0669e40b6357132fda1d81
maratnasyrov <marat.nasirov@flatstack.com>
2015-05-28 23:55:56 +0300

    add new style for sign_in part 1

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,10 +1,12 @@
 def CommentPolicy
+  attr_reader :comment
+
   def initialize(user, comment)
     @user = user
     @comment = comment
   end
 
   def manage?
-    @comment.user == @user
+    comment.user == @user
   end
 end
